f0c86fd4e8b878f80d92928dfba73161025c8eb7
ithachioji08 <146053692+ithachioji08@users.noreply.github.com>
2023-11-30 11:04:19 +0900

    Add files via upload

--- ECsight_index_model.php
+++ ECsight_index_model.php
@@ -2,11 +2,11 @@
 require_once 'ECsight_common_model.php';
 
 function get_user_list($pdo,$user_name,$password){
-  $sql    = 'select user_id, user_name,password from ec_user where user_name ='.$user_name.' and password ='.$password;
+  $sql    = 'select user_id, user_name,password from ec_user where user_name ="'.$user_name.'" and password ="'.$password.'"';
   $result = get_sql_result($pdo,$sql);
   if (count($result) ==0){
     return 'none';
-  }else if($result[0]['user_id']==0){
+  }else if($result[0]['user_id']==1){
     return "admin";
   }else{
     return 'user';
