7dc4cbce5495d97657c5899a90573459d378896c
ithachioji08 <146053692+ithachioji08@users.noreply.github.com>
2023-11-30 14:23:23 +0900

    Add files via upload

--- ECsight_management_view.php
+++ ECsight_management_view.php
@@ -5,13 +5,13 @@ include "ECsight_header.php"
 ?>
 	<div class="regist">
 		<form method="post">
-			<p>商品名　:<input type="text" name="product_name"></p>
-			<p>価格　　:<input type="text" name="price"></p>
+			<p>商品名　　:<input type="text" name="product_name"></p>
+			<p>価格　　　:<input type="text" name="price"></p>
 			<p>
-				<label for="counter">個数　　:</label>
-				<input type="number" id="counter" name="count" step="1" min="1">
+				<label for="counter">個数　　　:</label>
+				<input type="number" id="counter" name="count" step="1" min="1" value="1">
 			</p>
-			<p>商品画像:<input type="file" name="image"></p>
+			<p>商品画像　:<input type="file" name="image"></p>
 			<p>ステータス:
 				<select name="status">
 					<option>公開</option>
--- management.php
+++ management.php
@@ -5,11 +5,14 @@ require_once '../../include/config/const.php';
  
 //$pdo = get_connection();
 
-if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['user_name']) && isset($_POST['password'])) {
-	$name = $_POST['user_name'];
-	$password = $_FILES['password'];
-	brankCheck($name,$password);
-	switch(get_user_list($pdo,$_POST['user_name'],$_POST['password'])){
+if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['product_name'])) {
+	$name   = $_POST['user_name'];
+	$price  = $_POST['price'];
+	$count  = $_POST['count'];
+	$image  = $_FILES['image'];
+	$status = getStatus($_POST['status']);
+	brankCheck($name,$price,$image);
+	switch(insert_product($pdo,$name,$price,$count,$image,$status)){
 		case 'none';
 			header("Location: index.php?message=mismatch");
 			break;
@@ -36,8 +39,16 @@ if($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['message'])) {
 // View(view.php）読み込み
 include_once '../../include/view/ECsight_management_view.php';
 
-function brankCheck($name,$password){
-	if($name=='' || $password==''){
+function getStatus($status){
+	if ($status == '公開'){
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+function brankCheck($name,$price,$image){
+	if($name=='' || $price=='' || $image = ''){
 		header("Location: index.php?message=blank");
 		exit();
 	}
